a690f62e114133c10a119ad7189b3a3f5b4af298
Jason Slavin <slavin@adobe.com>
2025-06-11 12:36:50 -0700

    Reverse

--- tools/nobu-test/crawl-test.js
+++ tools/nobu-test/crawl-test.js
@@ -1,30 +1,29 @@
 // eslint-disable-next-line import/no-unresolved
 import { crawl } from 'https://da.live/nx/public/utils/tree.js';
-// eslint-disable-next-line import/no-unresolved
-import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 
 const PATH = '/adobecom/da-bacom/products/analytics/';
 
 (async function init() {
   // Create cancel button
-  const { context, token } = await DA_SDK;
-  const DA_ORIGIN = 'https://admin.da.live';
+  const crawlTestSection = document.querySelector('.crawl-test');
+  const button = document.createElement('button');
+  button.innerText = 'Cancel';
+  crawlTestSection.append(button);
 
-  const fullPath = `${DA_ORIGIN}/list${PATH}`;
-
-  const HEADERS = {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
+  // Create the callback to fire when a file is returned
+  const callback = (file) => {
+    button.insertAdjacentHTML('afterend', `<p>${file.path}</p>`);
   };
 
-  const opts = {
-    method: 'GET',
-    headers: HEADERS,
-  };
+  // Start the crawl
+  const { results, getDuration, cancelCrawl } = crawl({ path: PATH, callback, throttle: 10 });
 
-  const resp = await fetch(fullPath, opts);
-  if (resp.ok) {
-    const json = await resp.json();
-    console.log(json);
-  }
+  // Asign the cancel button the cancel event
+  button.addEventListener('click', cancelCrawl);
+
+  // Await the results to finish
+  await results;
+
+  // Add the duration after the results are finished
+  button.insertAdjacentHTML('beforebegin', `<p>${getDuration()}</p>`);
 }());
