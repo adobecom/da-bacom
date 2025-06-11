6dce9394e7de4441ff9868e0c34a765b159a836f
Jason Slavin <slavin@adobe.com>
2025-06-11 12:45:35 -0700

    Seeing if an authorized call will set ims permissions for the crawl call

--- tools/nobu-test/crawl-test.js
+++ tools/nobu-test/crawl-test.js
@@ -1,10 +1,35 @@
 // eslint-disable-next-line import/no-unresolved
 import { crawl } from 'https://da.live/nx/public/utils/tree.js';
+// eslint-disable-next-line import/no-unresolved
+import DA_SDK from 'https://da.live/nx/utils/sdk.js';
 
 const PATH = '/adobecom/da-bacom/products/analytics/';
 
 (async function init() {
   // Create cancel button
+  const { context, token } = await DA_SDK;
+
+  const DA_ORIGIN = 'https://admin.da.live';
+
+  const fullPath = `${DA_ORIGIN}/list${PATH}`;
+
+  const HEADERS = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${token}`,
+  };
+
+  const opts = {
+    method: 'GET',
+    headers: HEADERS,
+  };
+
+  const resp = await fetch(fullPath, opts);
+  if (resp.ok) {
+    const json = await resp.json();
+    console.log(json);
+  }
+
+  // Crawl below
   const crawlTestSection = document.querySelector('.crawl-test');
   const button = document.createElement('button');
   button.innerText = 'Cancel';
