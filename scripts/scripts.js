97a0c6df8d7afdad6970eed534f343a930710889
Jason Slavin <slavin@adobe.com>
2026-01-08 12:07:25 -0800

    Test

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -164,7 +164,7 @@ const loadStyle = (path) => {
 };
 
 const fetchy = async () => {
-  const resp = await fetch('https://business.stage.adobe.com/this/page/does/not/exist');
+  const resp = await fetch('https://business.stage.adobe.com/products/experience-platform/planning-and-measurement.html');
   if (!resp.ok) console.log('issue with request', resp, resp.body);
   console.log('resp ok', resp, resp.body);
 };
